d0931a07756da5aed6997f8bfbda56cddac2f09d
Kyle Raterta <raterta1112@gmail.com>
2025-04-12 21:53:12 +0800

    Deployment

--- routes/web.php
+++ routes/web.php
@@ -2,27 +2,30 @@
 
 /** @var \Laravel\Lumen\Routing\Router $router */
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It is a breeze. Simply tell Lumen the URIs it should respond to
-| and give it the Closure to call when that URI is requested.
-|
-*/
+$router->get('/', function () use ($router) {
+    return $router->app->version();
+});
 
-$router->group(['prefix' => 'api'], function () use ($router) {
+// Group the gateway routes and protect them with the new 'gateway.auth' middleware.
+$router->group(['middleware' => 'gateway.auth'], function () use ($router) {
+    // API GATEWAY ROUTES FOR SITE1 USERS
     $router->get('/users1', 'User1Controller@index');
-    $router->get('/users1/{id}', 'User1Controller@show');
     $router->post('/users1', 'User1Controller@add');
+    $router->get('/users1/{id}', 'User1Controller@show');
     $router->put('/users1/{id}', 'User1Controller@update');
+    $router->patch('/users1/{id}', 'User1Controller@update');
     $router->delete('/users1/{id}', 'User1Controller@delete');
 
+    // API GATEWAY ROUTES FOR SITE2 USERS
     $router->get('/users2', 'User2Controller@index');
-    $router->get('/users2/{id}', 'User2Controller@show');
     $router->post('/users2', 'User2Controller@add');
+    $router->get('/users2/{id}', 'User2Controller@show');
     $router->put('/users2/{id}', 'User2Controller@update');
+    $router->patch('/users2/{id}', 'User2Controller@update');
     $router->delete('/users2/{id}', 'User2Controller@delete');
+});
+
+// Other routes (if needed) that use a different auth middleware.
+$router->group(['middleware' => 'auth'], function () use ($router) {
+    $router->get('/users/me', 'UserController@me');
 });
